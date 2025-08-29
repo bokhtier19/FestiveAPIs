8ff11449092d316c08f10449a55ec9ccc5ea3944
ELIUS BOKHTIER <68582551+bokhtier19@users.noreply.github.com>
2025-08-29 23:36:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Filters from "./components/Filters";
 import FestivalList from "./components/FestivalList";
 import React from "react";
 
-const API_KEY = process.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 const YEAR = new Date().getFullYear();
 
 function App() {
